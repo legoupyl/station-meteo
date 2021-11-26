2c3ea83c47cd8a00a3bae0c2d42d4809a2ffc618
legoupyl <33391012+legoupyl@users.noreply.github.com>
2021-11-26 18:59:43 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get install apt-utils
+sudo apt-get install libsqlite3-dev
+sudo apt-get install sqlite3
+sudo apt-get install python3-pip
+python -m pip install pyephem
+python -m pip install sqlalchemy
+#sudo apt-get install python-rpi.gpio   notworking
+sudo apt-get install git-core
+python -m pip install wiringpi
+python -m pip install apscheduler
+python -m pip install pySerial
+python -m pip install smbus2
+python -m pip install RPi.GPIO
+python -m pip install paho-mqtt
+python -m pip install wget
+
+
