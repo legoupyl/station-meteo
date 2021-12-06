046ff0fde9f6ab1483a62cada3a1efadd37fc398
legoupyl <33391012+legoupyl@users.noreply.github.com>
2021-12-06 10:09:12 +0100

    Update install.sh

--- mqtt-db-logger/install.sh
+++ mqtt-db-logger/install.sh
@@ -1,1 +1,14 @@
 docker build . xavfan/mqtt-db-logger
+docker run --name mqtt-db-logger mqtt-db-logger bash
+
+
+
+
+cat <<EOT >> mqtt_db_logger_conf.py
+mqtt_brocker="####.cloudapp.net"
+topic="#####"
+db_srvname="#####"
+db_name="#####"
+db_username="###"
+db_user_pwd="####"
+EOT
