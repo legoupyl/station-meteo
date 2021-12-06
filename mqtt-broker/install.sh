545ce506844ce36929365d046e37b6f86d4fc01e
legoupyl <33391012+legoupyl@users.noreply.github.com>
2021-12-06 16:03:24 +0100

    Update install.sh

--- mqtt-broker/install.sh
+++ mqtt-broker/install.sh
@@ -1,1 +1,1 @@
-docker run --restart unless-stopped -p 1883:1883 --name mqtt-broker eclipse-mosquitto
+docker run --restart unless-stopped -p 1883:1883 -v /data/mqtt-broker:/data/ --name mqtt-broker eclipse-mosquitto
